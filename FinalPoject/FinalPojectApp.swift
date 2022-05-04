e0a9b92c7f0e6f1f75f5d176aec6749ced41ccf5
BananaChiu <agimz0330@gmail.com>
2022-05-04 10:59:08 +0800

    Initial Commit

---
+++ FinalPoject/FinalPojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalPojectApp.swift
+//  FinalPoject
+//
+//  Created by User23 on 2022/5/4.
+//
+
+import SwiftUI
+
+@main
+struct FinalPojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
